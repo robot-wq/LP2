4a6396b349194d5a71edf62de74f75f621c1df0f
Takeshi <yoshikawapaulo46@gmail.com>
2026-05-29 22:19:00 -0300

    Add files via upload

---
+++ PAluno01/PAluno01/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PAluno01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lstnota = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(12, 2);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(156, 116);
+            this.btnExecutar.TabIndex = 0;
+            //this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+//            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(12, 279);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(156, 109);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lstnota
+            // 
+            this.lstnota.FormattingEnabled = true;
+            this.lstnota.ItemHeight = 20;
+            this.lstnota.Location = new System.Drawing.Point(568, 124);
+            this.lstnota.Name = "lstnota";
+            this.lstnota.Size = new System.Drawing.Size(377, 264);
+            this.lstnota.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 621);
+            this.Controls.Add(this.lstnota);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox lstnota;
+    }
+}
+
---
+++ PAluno01/PAluno01/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PAluno01
+{
+    public partial class Form1 : Form
+    {
+        double[,] nota = new double[0, 10];
+        double[] média = new double[10];
+        double soma;
+        string[] professor = { "aluno1", "aluno2", "aluno3" };
+        private object lstBox_Valores;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public object Interaction { get; private set; }
+
+        private int GetV()
+        {
+            return lstnota.Items.Add($"média: {média}");
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e, int v)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+                    string input = ($"Digite o valor da nota {j + 1} de {professor[i]}");
+
+                    if (!double.TryParse(input, out nota[i, j]))
+                    {
+                        MessageBox.Show("Apenas valores numéricos são aceitos");
+                        j--;
+                    }
+                    média[i] += nota[i, j];
+                }
+                soma += média[i];
+            }
+
+
+            for (int k = 1; k < 10; k++)
+            {
+                lstnota.Items.Add($"{média[k]}: {professor[k]}");
+            }
+        }
+               
+           
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            btnLimpar.Select();
+            Array.Clear(nota, 0, nota.Length);
+            Array.Clear(média, 0, média.Length);
+            média = null;
+        }
+    }
+}
+
+//obs:não sei porque não está indo a compilação
+ 
